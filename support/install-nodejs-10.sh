85d21b2d9a4cf989c4bdc0fdb51e6d349fe3fee4
activehack <danny@activehack.com>
2018-12-21 11:15:32 +0800

    Create install-nodejs-10.sh

---
+++ support/install-nodejs-10.sh
@@ -0,0 +1,13 @@
+echo "Begin installing node v10"
+URL=https://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-x64.tar.xz
+OUTFILE=/tmp/node.tar.xz
+SETUPDIR=/tmp/node-setup
+mkdir -p ${SETUPDIR}
+wget -O ${OUTFILE} ${URL}
+tar -xvf ${OUTFILE} --directory ${SETUPDIR}
+mv ${SETUPDIR}/node-*-x64/bin/ /usr/local/bin/
+mv ${SETUPDIR}/node-*-x64/include/ /usr/local/include/
+mv ${SETUPDIR}/node-*-x64/lib/ /usr/local/lib/
+mv ${SETUPDIR}/node-*-x64/share/ /usr/local/share/
+rm -rf ${OUTFILE} ${SETUPDIR}
+echo "Node v10 is installed"
