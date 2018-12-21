b77d1cfa23a071530c9b0fc6c43954331be986b0
activehack <danny@activehack.com>
2018-12-21 11:53:12 +0800

    Update install-nodejs-10.sh

--- support/install-nodejs-10.sh
+++ support/install-nodejs-10.sh
@@ -5,9 +5,9 @@ SETUPDIR=/tmp/node-setup
 mkdir -p ${SETUPDIR}
 wget -O ${OUTFILE} ${URL}
 tar -xf ${OUTFILE} --directory ${SETUPDIR}
-cp -rv ${SETUPDIR}/node-*-x64/bin/* /usr/local/bin/
-cp -r ${SETUPDIR}/node-*-x64/include/* /usr/local/include/
-cp -r ${SETUPDIR}/node-*-x64/lib/* /usr/local/lib/
-cp -r ${SETUPDIR}/node-*-x64/share/* /usr/local/share/
+cp -rv ${SETUPDIR}/node-*-x64/bin/* /usr/bin/
+cp -r ${SETUPDIR}/node-*-x64/include/* /usr/include/
+cp -r ${SETUPDIR}/node-*-x64/lib/* /usr/lib/
+cp -r ${SETUPDIR}/node-*-x64/share/* /usr/share/
 rm -rf ${OUTFILE} ${SETUPDIR}
 echo "Node v10 is installed"
