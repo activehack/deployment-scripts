521b1218735e9b64a8bd03974e4137f527a5b2a8
activehack <danny@activehack.com>
2018-12-21 08:45:20 +0800

    Update deploy.sh

--- gcloud/maven/3/jdk/8/deploy.sh
+++ gcloud/maven/3/jdk/8/deploy.sh
@@ -1,7 +1,11 @@
 GOOGLE_KEY_FILE=/opt/client-keyfile.json
 cd ${BITBUCKET_CLONE_DIR}
+echo -n "Creating client key json file..."
 echo ${GOOLE_KEY_DATA} | base64 -d > ${GOOGLE_KEY_FILE}
+echo " [OK]"
+echo "Authenticating google cloud service..."
 gcloud auth activate-service-account --key-file ${GOOGLE_KEY_FILE}
+echo " [OK"
 echo "Check current directory:"
 pwd
 mvn clean -B verify -P DEV -DskipTests appengine:deploy
