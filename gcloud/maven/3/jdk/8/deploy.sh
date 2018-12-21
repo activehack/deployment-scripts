ec716d828cdca0d43807d7d644d02d3838de4b91
activehack <danny@activehack.com>
2018-12-21 12:43:16 +0800

    Update deploy.sh

--- gcloud/maven/3/jdk/8/deploy.sh
+++ gcloud/maven/3/jdk/8/deploy.sh
@@ -1,8 +1,6 @@
 GOOGLE_KEY_FILE=/opt/client-keyfile.json
 cd ${BITBUCKET_CLONE_DIR}
-echo "Creating client key json file..."
 echo ${GOOLE_KEY_DATA} | base64 -d > ${GOOGLE_KEY_FILE}
-echo "Authenticating google cloud service..."
 gcloud auth activate-service-account --key-file ${GOOGLE_KEY_FILE} --project=${GOOGLE_PROJECT_ID}
 gcloud -v
 echo "Google cloud environment is ready for: nodejs, python and java. Enjoy :)"
