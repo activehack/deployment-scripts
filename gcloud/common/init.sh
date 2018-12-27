f6d9e020bf7278bf6aecf080bd2708dbd31b481c
activehack <danny@activehack.com>
2018-12-27 10:12:30 +0800

    Update init.sh

--- gcloud/common/init.sh
+++ gcloud/common/init.sh
@@ -1,6 +1,6 @@
 GOOGLE_KEY_FILE=/opt/client-keyfile.json
 cd ${BITBUCKET_CLONE_DIR}
-echo ${GOOLE_KEY_DATA} | base64 -d > ${GOOGLE_KEY_FILE}
+echo ${GOOGLE_KEY_DATA} | base64 -d > ${GOOGLE_KEY_FILE}
 gcloud auth activate-service-account --key-file ${GOOGLE_KEY_FILE} --project=${GOOGLE_PROJECT_ID}
 gcloud -v
 echo "Google cloud environment is ready for: nodejs, python and java. Enjoy :)"
