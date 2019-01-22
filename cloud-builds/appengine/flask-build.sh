944d12dc686193736bca4315457fe61f84ec8b9c
activehack <danny@activehack.com>
2019-01-22 17:36:48 +0800

    Update flask-build.sh

--- cloud-builds/appengine/flask-build.sh
+++ cloud-builds/appengine/flask-build.sh
@@ -1,6 +1,6 @@
-[ ! -e appengine_config.py ] || mkdir -p lib
+[ ! -e appengine_config.py ] || mkdir -v -p lib
 [ ! -e requirements.txt ] || pip install --upgrade -t lib -r requirements.txt
-[ -d lib ] || rm -f appengine_config.py
+[ -d lib ] || rm -v -f appengine_config.py
 gcloud app deploy --quiet
 [ ! -e dispatch.yaml ] || gcloud app deploy dispatch.yaml --quiet
 [ ! -e index.yaml ] || gcloud datastore indexes create index.yaml --quiet
