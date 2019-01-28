a77cdf4dd2fb8f3dcc27834ce566003d436e6a26
activehack <danny@activehack.com>
2019-01-28 14:52:01 +0800

    Create webpack.config.js

---
+++ webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use:{
+          loader: "babel-loader",
+          options:{
+            presets:[
+            ["env",{
+              "targets":{
+                "browsers":["since 2015","safari >= 7","ie >= 8","Firefox ESR"]
+              }
+            }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions:{
+        compress:true,
+        ecma:5,
+        output:{
+          comments:false
+        }
+      }
+    })]
+  },
+  entry: '/opt/src/index.js',
+  output: {
+    filename: 'main.js',
+    path: '/opt/dist'
+  }
+};
